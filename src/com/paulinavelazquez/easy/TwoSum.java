704c9dd504aeca535201c002507895256e2e14c4
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-03-22 16:24:20 -0400

    4th commit

--- src/com/paulinavelazquez/easy/TwoSum.java
+++ src/com/paulinavelazquez/easy/TwoSum.java
@@ -1,4 +1,16 @@
 package com.paulinavelazquez.easy;
 
 public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 15, 20, 15};
+        int target = 9;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == (target - nums[j])) {
+                    System.out.println("Answer: "+i+j);
+                }
+            }
+        }
+    }
 }
